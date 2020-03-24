dd7105ebe6d3b947ba59c5fd7bd7a6e0ed295d48
Juliano Acuña <juliano.acunia@hotmail.com>
2020-03-24 19:15:43 -0300

    PublicHome component

---
+++ src/components/publicHome.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import Products from './Products'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { Link } from 'react-router-dom'
+
+class PublicHome extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>---Welcome to t-shirts store!---</h1>
+        <div id='login2'>
+          <div className='buttonmenu'>
+            <Link to='/register'>Sign up</Link>
+          </div>
+          <div className='buttonmenu'>
+            <Link to='/login'>Login</Link>
+          </div>
+        </div>
+        <hr />
+        <div className='row'>
+          <div className='col-md-8'>
+            <Filter />
+            <Products />
+          </div>
+          <div className='col-md-4'>
+            <Basket />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    products: state.products,
+    isLoading: state.isLoading,
+    isAuth: state.isAuth
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PublicHome)

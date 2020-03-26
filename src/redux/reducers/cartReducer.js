2ad5190b73e88d1f25a849417dc24ba27d748dff
Juliano Acuña <juliano.acunia@hotmail.com>
2020-03-26 17:37:32 -0300

    Add cart reducer

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -1,3 +1,15 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/types'
 
 const initialState = { items: [] }
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+      case ADD_TO_CART:
+        return {
+          items: action.payload.cartItems
+        }
+      default:
+        return state
+    }
+  }
+  

6e9cd3a294899afb83476ab4b21d2ac9e3a731fd
Juliano Acuña <juliano.acunia@hotmail.com>
2020-03-26 17:34:10 -0300

    Add to cart action and types

---
+++ src/redux/actions/cartActions.js
@@ -0,0 +1,23 @@
+import { ADD_TO_CART, REMOVE_FROM_CART } from './types'
+
+// ADD TO CART METHOD
+export const addToCart = (items, product) => dispatch => {
+    const cartItems = items.slice()
+    let productAlreadyInCart = false
+    cartItems.forEach(cp => {
+      if (cp._id === product._id) {
+        cp.count += 1
+        productAlreadyInCart = true
+      }
+    })
+    if (!productAlreadyInCart) {
+      cartItems.push({ ...product, count: 1 })
+    }
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
+    return dispatch({
+      type: ADD_TO_CART,
+      payload: {
+        cartItems
+      }
+    })
+}

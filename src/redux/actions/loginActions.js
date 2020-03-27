904f7919f35957e7098b65ed2e4df8112dc63926
Juliano Acuña <juliano.acunia@hotmail.com>
2020-03-27 20:43:08 -0300

    login action

---
+++ src/redux/actions/loginActions.js
@@ -0,0 +1,48 @@
+import {
+    LOGIN_USER_ERROR,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_PENDING,
+    IS_AUTH,
+    USER_LOGOUT,
+    FETCH_USERS,
+    ADD_USER_PENDING,
+    ADD_USER_SUCCESS,
+    ADD_USER_ERROR
+} from './types'
+
+//LOGIN USER ACCOUNT
+export const loginAccount = data => {
+    return dispatch => {
+      dispatch({
+        type: LOGIN_USER_PENDING
+      })
+      const options = {
+        timeout: 25000,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: document.getElementById('inputName').value,
+          password: document.getElementById('inputPassword').value
+        })
+      }
+      return fetch('http://localhost:5000/api/user/signIn', options)
+        .then(res => res.json())
+        .then(res => {
+          if (res.msg !== 'Invalid Email or password') {
+            console.log(res)
+            return dispatch({
+              type: LOGIN_USER_SUCCESS,
+              payload: res
+            })
+          } else {
+            return dispatch({
+              type: LOGIN_USER_ERROR,
+              payload: res.error
+            })
+          }
+        })
+    }
+}
+

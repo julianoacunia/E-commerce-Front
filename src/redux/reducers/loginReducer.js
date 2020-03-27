5b222bf0932880c57c754c780978e863ca91867a
Juliano Acuña <juliano.acunia@hotmail.com>
2020-03-27 20:53:20 -0300

    Fetch users reducer

--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -53,6 +53,11 @@ export default function(state = initialState, action) {
           message: action.payload,
           failedLogin: true
         }
+        case FETCH_USERS:
+        return {
+          ...state,
+          users: action.payload
+        }
         default:
             return state
     }

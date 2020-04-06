91f3fb717570a7d8fdfd03493188329d3bea736e
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-06 16:58:15 -0300

    Add categorie reducer

--- src/redux/reducers/categorieReducer.js
+++ src/redux/reducers/categorieReducer.js
@@ -23,6 +23,27 @@ export default function ( state = initialState, action) {
             items: action.payload,
             adminActions: false
         }
+        case ADD_CATEGORIE_PENDING:
+        return {
+            ...state,
+            isLoading: true
+        }
+        case ADD_CATEGORIE_SUCCESS: {
+        const newCategorie = action.payload.categorie.data
+        const categories = [...state.items, newCategorie]
+            return {
+                ...state,
+                isLoading: false,
+                items: categories
+            }
+        }
+        case ADD_CATEGORIE_ERROR:
+        return {
+            ...state,
+            isLoading: false,
+            error: action.error,
+            message: action.payload.message
+        }
         default:
         return state
     }
